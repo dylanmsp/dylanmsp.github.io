e36d63c196d915d335096d94ece6a37f189c69cd
dylanmsp <47540559+dylanmsp@users.noreply.github.com>
2019-02-22 21:50:10 -0500

    contact form

--- contact_form.php
+++ contact_form.php
@@ -1,52 +1,39 @@
 <?php
-     $name = $_POST['name'];
-     $email = $_POST['email'];
-     $message = $_POST['message'];
-     $contact = $_POST['contact'];
-     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          
+          $name = strip_tags(trim($_POST["name"]));
+          $name = str_replace(array("\r","\n"),array(" "," "),$name);
+          $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+          $message = trim($_POST["message"]);
 
-     if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          if (!preg_match("/^[0-9]{10}$/", $contact)) {
-               // echo "Please put in a valid phone number, no dashes”;
-          } else {
-               // $subjectCust = ‘Thank you for your message ‘.$name;
-               // $subjectOwner = ‘Website message from: ‘ . $name;
-               // $headers = ‘MIME-Version: 1.0’ . “\r\n”;
-               // $headers .= ‘Content-type: text/html; charset=iso-8859-1’ . “\r\n”;
-               // $headers .= ‘From:’ . $email. “\r\n”;
-               
-               // $template2Customer = ‘Hello ‘ . $name . ‘,’
-               // . ‘Thank you for contacting us.’
-               // . ‘Your message to us: ‘
-               // . ‘Full Name:’ . $name . ”
-               // . ‘Email Address:’ . $email . ”
-               // . ‘Telephone:’ . $contact . ”
-               // . ‘Message:’ . $message . ”
-               // . ‘This is a confirmation mail.’
-               // . ”
-               // . ‘We will contact you as soon as possible.’
-               // . ‘http://www.msptechservices.com’;
-               
-               // $template2SiteOwner = ‘Website message from: ‘
-               // . ‘Full Name: ‘ . $name . ”
-               // . ‘Email Address: ‘ . $email . ”
-               // . ‘Telephone: ‘ . $contact . ”
-               // . ‘Message: ‘ . $message . ”
-               // . ”
-               // . ‘This message was sent from: http://www.msptechservices.com‘;
-               
-               // $sendmessage2Cust = $template2Customer;
-               // $sendmessage2SiteOwner = $template2SiteOwner;
-               
-               // $sendmessage2Cust = wordwrap($sendmessage2Cust, 70);
-               // $sendmessage2SiteOwner = wordwrap($sendmessage2SiteOwner, 70);
-               
-               // mail($email, $subjectCust, $sendmessage2Cust, $headers);
-               // mail(“dylan@msptechservices.com”, $subjectOwner, $sendmessage2SiteOwner, $headers);
-               
-               // echo “Your message has been received. We will contact you soon.”;
+          if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+               http_response_code(400);
+               echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+               exit;
           }
+
+          $recipient = "dylan@msptechservices.com";
+          $subject = "Website message from $name";
+
+          $email_content = "Name: $name\n";
+          $email_content .= "Email: $email\n\n";
+          $email_content .= "Message:\n$message\n";
+
+          $email_headers = "From: $name <$email>";
+
+          if (mail($recipient, $subject, $email_content, $email_headers)) {
+
+          http_response_code(200);
+          echo "Thank you! Your message has been sent. We will contact you soon.";
+          } else {
+
+               http_response_code(500);
+               echo "Oops! Something went wrong and we couldn't send your message.";
+          }
+
      } else {
-          // echo “*** INVALID EMAIL ***”;
+
+          http_response_code(403);
+          echo "There was a problem with your submission. Please try again.";
      }
 ?>
--- scripts/msptech.js
+++ scripts/msptech.js
@@ -1,26 +1,56 @@
-$(document).ready(function() {
-     $("#submit").click(function() {
-          var name = $("#name").val();
-          var email = $("#email").val();
-          var message = $("#message").val();
-          var contact = $("#contact").val();
-          $("#returnmessage").empty();
+$(function() {
+     
+     // Get the form.
+     var form = $('#ajax-contact');
 
-          if (name == '' || email == '' || contact == '') {
-               alert("Please complete all *** REQUIRED *** fields.");
-          } else {
+     // Get the messages div.
+     var formMessages = $('#form-messages');
 
-          $.post("contact_form.php", {
-          name1: name,
-          email1: email,
-          message1: message,
-          contact1: contact
-          }, function(data) {
-               $("#returnmessage").append(data);
-               if (data == "Thank you! We will contact you within 48 hours.") {
-                    $("#form")[0].reset();
+     // Set up an event listener for the contact form.
+     $(form).submit(function(event) {
+     
+          // Stop the browser from submitting the form.
+          event.preventDefault();
+
+          // Serialize the form data.
+          var formData = $(form).serialize();
+          
+          // Submit the form using AJAX.
+          $.ajax({
+               type: 'POST',
+               url: $(form).attr('action'),
+               data: formData
+          })
+
+          .done(function(response) {
+
+               // Make sure that the formMessages div has the 'success' class.
+               $(formMessages).removeClass('error');
+               $(formMessages).addClass('success');
+
+               // Set the message text.
+               $(formMessages).text(response);
+
+               // Clear the form.
+               $('#name').val('');
+               $('#email').val('');
+               $('#message').val('');
+          })
+          
+          .fail(function(data) {
+    
+               // Make sure that the formMessages div has the 'error' class.
+               $(formMessages).removeClass('success');
+               $(formMessages).addClass('error');
+
+               // Set the message text.
+               if (data.responseText !== '') {
+                    $(formMessages).text(data.responseText);
+               } else {
+                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
                }
           });
-          }
+          
      });
+     
 });
